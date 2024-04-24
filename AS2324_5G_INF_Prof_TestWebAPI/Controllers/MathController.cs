2f02aaf76aceb8cce9b5f6363ce0d1268663d7cc
Filippo Albertini <filippo@filippoalbertini.it>
2024-04-24 08:49:47 +0200

    feat: add method GetDivision

---
+++ AS2324_5G_INF_Prof_TestWebAPI/Controllers/MathController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AS2324_5G_INF_Prof_TestWebAPI.Controllers
+{
+    public class MathController : Controller
+    {
+        [HttpGet("GetDivision")]
+        public JsonResult GetDivision(float numeratore, float denominatore)
+        {
+            string status_result = "OK";
+            double res;
+
+            if (denominatore == 0)
+            {
+                status_result = "KO";
+                res = 0;
+            }
+            else
+            {
+                res = numeratore / denominatore;
+            }
+
+            return Json(new { output = res, status = status_result });
+        }
+    }
+
+
+}
