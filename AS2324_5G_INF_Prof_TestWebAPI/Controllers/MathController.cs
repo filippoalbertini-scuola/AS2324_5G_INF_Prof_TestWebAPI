e82dae0102a52baff13386447d8a7f42e1f691b6
Filippo Albertini <filippo@filippoalbertini.it>
2024-04-24 09:10:39 +0200

    feat: add GetFattoriale method to calculate Factorial number

--- AS2324_5G_INF_Prof_TestWebAPI/Controllers/MathController.cs
+++ AS2324_5G_INF_Prof_TestWebAPI/Controllers/MathController.cs
@@ -22,6 +22,30 @@ namespace AS2324_5G_INF_Prof_TestWebAPI.Controllers
 
             return Json(new { output = res, status = status_result });
         }
+
+        [HttpGet("GetFattoriale")]
+        public JsonResult GetFattoriale(int numero)
+        {
+            string status_result = "OK";
+            string message = "Valore calcolabile";
+            int res;
+
+            if (numero < 0)
+            {
+                status_result = "KO";
+                res = 0;
+                message = "Valore non calcolabile perchè il numero è inferiore a 0";
+            }
+            else
+            {
+                res = 1;
+
+                for (int i = numero; i > 0; i--)
+                    res *= i;
+            }
+
+            return Json(new { output = res, status = status_result, message = message });
+        }
     }
 
 
